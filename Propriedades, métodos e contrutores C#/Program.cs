25d826691d5c88d014d8158a51be27ce67bace97
Weydson Fernandes <83516828+FerWeydson@users.noreply.github.com>
2023-01-16 13:34:38 -0300

    Serializando e deserializando Json

--- Propriedades, métodos e contrutores C#/Program.cs
+++ Propriedades, métodos e contrutores C#/Program.cs
@@ -1,24 +1,101 @@
 ﻿using Propriedades__métodos_e_contrutores_C_.Models;
+using Newtonsoft.Json;
+
+
+
+
+
+
+// Serializar uma lista em Json
+
+
+
+// DateTime dataAtual = DateTime.Now;
+// List<Vendas> listaVenda = new List<Vendas>();
+
+// Vendas v1 = new Vendas(1, "Lápis", 2.00M, dataAtual);
+// Vendas v2 = new Vendas(2, "Caneta", 1.25M, dataAtual);
+
+// listaVenda.Add(v1);
+// listaVenda.Add(v2);
+
+// string serializado = JsonConvert.SerializeObject(listaVenda, Formatting.Indented);
+// File.WriteAllText("Arquivos/vendas.json", serializado);
+
+// System.Console.WriteLine(serializado);
+
+
+
+// Deserializando 
+
+
+// string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
+// List<Vendas> listavenda = JsonConvert.DeserializeObject<List<Vendas>>(conteudoArquivo);
+
+// foreach(Vendas venda in listavenda){
+//     System.Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, " +
+//                             $"Preco: {venda.Preco}, Data venda: {venda.DataVenda.ToString("dd/MM/yyyy HH:mm")}");
+// }
+
+
+
+
+
+
+
+
+// Tuplas
+
+// Usando metodo
+
+// TuplaEmMetodo arquivo = new TuplaEmMetodo();
+// var(sucesso, Linhas, QuantidadeLinhas) = arquivo.LerArquivo("Arquivos/Leitura.txt");
+
+// if (sucesso) {
+//     System.Console.WriteLine($"Quantidade de linhas: {QuantidadeLinhas}");
+//     foreach(string linha in Linhas) {
+//         System.Console.WriteLine(linha);
+//     }
+// }else {
+//     System.Console.WriteLine("Não foi possivel ler arquivo");
+// }
+
+
+
+// Sintaxes Tupla
+
+// (int Id, string Nome, string Sobrenome) tupla = (1, "weydson", "fernandes");
+// ValueTuple<int, string, string> tupla2 = (2, "Weslley", "Henrique");
+// var tupla3 = Tuple.Create((3, "asdasdasd", "Henraskdjhafuitsdue"));
+
+// System.Console.WriteLine(tupla.Nome);
+// System.Console.WriteLine(tupla2);
+
+
 
 
 // Coleções
 
+
+
+
 // Dictionary - chave-valor armazenados sem ordem especifica
 
-Dictionary<string, string> estados = new Dictionary<string, string>();
 
-estados.Add("PE", "Pernambuco");
-estados.Add("RJ", "Rio de Janeiro");
-estados.Add("SP", "São Paulo");
+// Dictionary<string, string> estados = new Dictionary<string, string>();
 
-foreach(var item in estados){
-    System.Console.WriteLine(item);
-}
-System.Console.WriteLine($"Removendo item {estados.Remove("PE")}");
+// estados.Add("PE", "Pernambuco");
+// estados.Add("RJ", "Rio de Janeiro");
+// estados.Add("SP", "São Paulo");
 
-foreach(var item in estados){
-    System.Console.WriteLine(item);
-}
+// foreach(var item in estados){
+//     System.Console.WriteLine(item);
+// }
+// System.Console.WriteLine($"Removendo item {estados.Remove("PE")}");
+
+// foreach(var item in estados){
+//     System.Console.WriteLine(item);
+// }
 
 
 
